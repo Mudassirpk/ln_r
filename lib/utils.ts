8d133b65b0a89ba7f593d4598fa23a35385329fb
Mudassirpk <mudassir.pk.mp@gmail.com>
2024-10-21 16:49:55 +0500

    add: ip address be url

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const httpCommon = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "http://172.25.18.33:3000/api",
 });
